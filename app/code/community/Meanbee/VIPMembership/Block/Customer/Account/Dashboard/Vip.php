e150f5988089e5c67d3f5cdec438898ecd545cf1
Ash Smith <ash.smith@meanbee.com>
2014-03-07 14:46:22 +0000

    Removed any remaining instances of 'Prime'

--- app/code/community/Meanbee/VIPMembership/Block/Customer/Account/Dashboard/Vip.php
+++ app/code/community/Meanbee/VIPMembership/Block/Customer/Account/Dashboard/Vip.php
@@ -1,8 +1,3 @@
 <?php
 
-class Meanbee_VIPMembership_Block_Customer_Account_Dashboard_Vip extends Mage_Customer_Block_Account_Dashboard {
-    public function isPrimeCustomer() {
-        $customerId = $this->getCustomer()->getId();
-        return Mage::helper('meanbee_vipmembership')->getCustomerVIPProfile($customerId) ? true : false;
-    }
-}
+class Meanbee_VIPMembership_Block_Customer_Account_Dashboard_Vip extends Mage_Customer_Block_Account_Dashboard { }
