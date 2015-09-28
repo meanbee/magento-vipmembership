957d644d0cf6799e3432985bd8b049f7ff9b6bbb
Ashley Wright <ashley.wright@meanbee.com>
2015-09-28 11:43:31 +0100

    VM-2: Makes 'vip_expiry' and 'vip_order_id' fields visible in Customer Management Uses suggested solution in issue VM-2.

---
+++ app/code/community/Meanbee/VIPMembership/sql/meanbee_vipmembership_customer_setup/upgrade-0.0.3-0.0.4.php
@@ -0,0 +1,20 @@
+<?php
+$installer = $this;
+/* @var $installer Mage_Customer_Model_Entity_Setup */
+
+$installer->startSetup();
+
+$sql = <<<EOQ
+
+INSERT INTO `customer_form_attribute`
+    SELECT 'adminhtml_customer',
+           `attribute_id`
+    FROM `eav_attribute`
+    WHERE `entity_type_id` = 1
+      AND `attribute_code` IN ('vip_expiry', 'vip_order_id');
+
+EOQ;
+
+$installer->run($sql);
+
+$installer->endSetup();
